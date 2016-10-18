7df650e47ef2c4b6b3e54fc88ef7864516e54dac
Quilljou <quillzhou@gmail.com>
2016-10-19 01:07:48 +0800

    try exlir

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,8 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var elixir = require('laravel-elixir');
+// var BrowserSync = require('laravel-elixir-browsersync2');
 
-gulp.task('sass',function(){
-    return gulp.src('./fesrc/sass/main.sass')
-            .pipe(sass({outputStyle: 'compressed'}).on('error',sass.logError))
-            .pipe(gulp.dest('./public/customer/css/'))
-})
-
-gulp.task('js', function() {
-    return gulp.src('./fesrc/js/*')
-        .pipe(gulp.dest('./public/customer/js/'))
-})
-
-
-gulp.task('watch',['sass','js'],function(){
-    gulp.watch('./fesrc/sass/**/*',['sass'])
-    gulp.watch('./fesrc/js/*',['js'])
-})
+elixir(function(mix) {
+    mix.sass('app.scss');
+});
--- resources/views/product/productCenter.blade.php
+++ resources/views/product/productCenter.blade.php
@@ -1,3 +1,83 @@
+<table class="table table-bordered table-hover table-condensed">
+    <thead>
+        <tr>
+            <td>
+                foo
+            </td>
+            <td>
+                bar
+            </td>
+            <td>
+                baz
+            </td>
+            <td>
+                for
+            </td>
+            <td>
+                bar
+            </td>
+            <td>
+                操作
+            </td>
+        </tr>
+    </thead>
+    <tbody >
+        <tr class="table-striped">
+            <td>
+                foo
+            </td>
+            <td>
+                bar
+            </td>
+            <td>
+                baz
+            </td>
+            <td>
+                for
+            </td>
+            <td>
+                bar
+            </td>
+            <td>
+                <button type="button" class="btn btn-danger">
+                  删除 <i class="fa fa-crosshairs"></i>
+                </button>
+                <button type="button" class="btn btn-primary">
+                  修改 <i class="fa fa-pencil"></i>
+                </button>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                foo
+            </td>
+            <td>
+                bar
+            </td>
+            <td>
+                baz
+            </td>
+            <td>
+                for
+            </td>
+            <td>
+                bar
+            </td>
+            <td>
+                <button type="button" class="btn btn-danger">
+                  删除 <i class="fa fa-crosshairs"></i>
+                </button>
+                <button type="button" class="btn btn-primary">
+                  修改 <i class="fa fa-pencil"></i>
+                </button>
+            </td>
+        </tr>
+    </tbody>
+</table>
+
+
+
+
 <ul>
     <li><a href="{{url('products')}}">查看所有商品信息</a></li>
 	<li><a href="{{url('products/create')}}">添加商品信息</a></li>
