bfc37fc2a21ab3941bb97f8a48218abd648d9775
lynasity <1151864810@qq.com>
2016-10-21 02:07:51 +0800

    latest

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -15,7 +15,7 @@ class AdminController extends Controller
 {
      public function __construct()
     {
-    //    $this->middleware('auth.admin:admin');
+       $this->middleware('auth.admin:admin');
     }
     public function index(){
     	return view('admin.home');
